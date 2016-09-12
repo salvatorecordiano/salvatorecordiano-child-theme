6eb49e7666d6f2da6f8458d17a4721d60aec1c7a
Salvatore Cordiano <github@parallel.it>
2016-09-12 09:27:15 +0200

    Added AMA box

---
+++ twentyfifteen-child/ask-me-anything.php
@@ -0,0 +1,6 @@
+
+<div id="ama" class="comments-area">
+	<h3 class="comment-reply-title">Questions?</h1>
+	<p>Have a question about this post or anything else? Ask away on <a href="https://twitter.com/parallelit" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>
+ Twitter</a> or in my <a href="https://github.com/salvatorecordiano/ama" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> AMA repo</a>.</p>
+</div>
